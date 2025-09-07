524dd85f185a8f6f6a44107ab824af0d97b11533
alwaysgot200 <ricky.ruixiwu@gmail.com>
2025-09-07 17:53:05 +0800

    v1.0.36

--- Files/__init__.py
+++ Files/__init__.py
@@ -1,4 +1,3 @@
-# 让 Files 成为一个包，便于通过 `from Files.encrypt_service import EncryptService` 导入
+# 让 Files 成为一个包，便于通过 `from Files.encrypt_service import EncryptService` 导入, test中使用
 from .encrypt_service import EncryptService
-
 __all__ = ["EncryptService"]
